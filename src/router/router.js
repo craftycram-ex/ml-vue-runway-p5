973f19cea4dc32fbf6d559189c2d73ad205bc8f9
craftycram <github@craftycram.net>
2021-04-26 15:09:14 +0200

    added bigan p5

--- src/router/router.js
+++ src/router/router.js
@@ -19,6 +19,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/bigGan.vue')
+  },
+  {
+    path: '/bigGanP5',
+    name: 'p5',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/bigGan-p5.vue')
   }
 ]
 
--- src/views/bigGan-p5.vue
+++ src/views/bigGan-p5.vue
@@ -1,26 +1,50 @@
 <template>
-  <Base64Image :imgData="imgData"/>
+  <vue-p5 v-if="loaded" class="p5" @setup="setup" @draw="draw" @mouseclicked="mouseclicked"></vue-p5>
 </template>
 
 <script>
-import Base64Image from '../components/base64Image.vue';
+  import VueP5 from 'vue-p5';
   export default {
     components: {
-      Base64Image,
+      "vue-p5": VueP5
     },
     data() {
       return {
         imgData: '',
+        img: null,
+        height: 400,
+        width: 600,
+        loaded: false,
       };
     },
     mounted() {
-      setInterval(() => {
-        this.axios.get('http://10.10.1.37:8000/data').then((response) => {
+      this.axios.get('http://10.10.1.37:8000/data').then((response) => {
         console.log(response.data)
-        this.imgData = response.data.generated_output;
+        if (response.data === null) return;
+        if (response.data.generated_output !== null) {
+          this.imgData = response.data.generated_output;
+        } else {
+          this.imgData = 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+        }
+        this.loaded = true;
       });
-      }, 250);
     },
+    methods: {
+      setup(sketch) {
+        this.width = sketch.displayWidth - 100;
+        this.height = sketch.displayHeight - 100;
+        sketch.createCanvas(this.width, this.height);
+        this.img = sketch.loadImage(this.imgData);
+      },
+      draw(sketch) {
+        sketch.background(0);
+        sketch.image(this.img, 0, 0, this.width, this.height);
+
+      },
+      mouseclicked() {
+        this.$emit('click-event');
+      },
+    }
   }
 </script>
 

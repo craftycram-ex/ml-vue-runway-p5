3090462d1f40ae2f83a082d045cdd8af84fd3d26
craftycram <github@craftycram.net>
2021-04-26 14:31:52 +0200

    added router

---
+++ src/views/p5.vue
@@ -0,0 +1,28 @@
+<template>
+  <vue-p5 v-on="this"></vue-p5>
+</template>
+
+<script>
+import VueP5 from 'vue-p5';
+
+export default {
+  mounted() {
+    this.$axios.get('localhost:8000').then((response) => {
+      console.log(response.data)
+    });
+  },
+  methods: {
+    setup(sketch) {
+      sketch.background('green');
+      sketch.text('Hello p5!', 20, 20);
+    }
+  },
+  render(h) {
+    return h(VueP5, {on: this});
+  }
+};
+</script>
+
+<style>
+
+</style>
